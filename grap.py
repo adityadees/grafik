b15a61ad9713c42967d17ac1d92582a4cf0f010c
AdityaDS <adityads@ymail.com>
2020-03-17 00:00:49 +0700

    set font

--- grap.py
+++ grap.py
@@ -1,15 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+params = {'legend.fontsize': 'x-large',
+          'figure.figsize': (15, 5),
+         'axes.labelsize': 'x-large',
+         'axes.titlesize':'x-large',
+         'xtick.labelsize':'x-large',
+         'ytick.labelsize':'x-large'}
 
+
+plt.rcParams.update(params)
 jumlah = 4
 
 data = [
-[99.9838, 97.9887, 97.8641, 99.9919],
-[98.0303, 79.8333, 77.4524, 98.9198],
-[99.9380, 93.3712, 95.3463, 98.9686],
-[98.1538, 69.2308, 69.2308, 99.0769],
-[100.0000, 100.0000, 100.0000, 100.0000],
+[round(99.9838,2), round(97.9887,2), round(97.8641,2), round(99.9919,2)],
+[round(98.0303,2), round(79.8333,2), round(77.4524,2), round(98.9198,2)],
+[round(99.9380,2), round(93.3712,2), round(95.3463,2), round(98.9686,2)],
+[round(98.1538,2), round(69.2308,2), round(69.2308,2), round(99.0769,2)],
+[round(100.0000,2), round(100.0000,2), round(100.0000,2), round(100.0000,2)],
 
 ]
 
@@ -35,7 +43,6 @@ plt.ylabel('Means Accuracy')
 plt.title('DNN Testing Graph')
 plt.xticks(index + lebar + lebar, ('Accuracy', 'Precision', 'Recall', 'F1-Score'))
 plt.legend(loc='lower left')
-
 def autolabel(rects):
     for rect in rects:
         height = rect.get_height()
@@ -43,6 +50,7 @@ def autolabel(rects):
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  
                     textcoords="offset points",
+                    fontsize='x-large',
                     ha='center', va='bottom')
 
 
